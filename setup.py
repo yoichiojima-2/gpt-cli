c3adc6a1faf34278388693727e09e82722993ef7
Yoichi Ojima <yoichiojima@gmail.com>
2023-10-29 20:11:13 +0900

    make it a package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="text_summarisation",
+    version="2023.10",
+    packages=find_packages(),
+    install_requires=["openai", "tqdm"],
+    entry_points={"console_scripts": ["text-summarisation = text_summarisation.main:main"]},
+)
--- text_summarisation/main.py
+++ text_summarisation/main.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 from argparse import ArgumentParser
 
-from split_text import main as split
-from merge_text import main as merge
-from summarise_split_text import main as summarise
+from .split_text import main as split
+from .merge_text import main as merge
+from .summarise_split_text import main as summarise
 
 
 def main(target):
@@ -18,7 +18,7 @@ def main(target):
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--target", "-t", type = str)
+    parser.add_argument("--target", "-t", type=str)
     args = parser.parse_args()
 
     main(args.target)
