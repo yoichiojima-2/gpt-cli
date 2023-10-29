e091bf70af3ff95e52b23864b93a9440d5a4dd4c
Yoichi Ojima <yoichiojima@gmail.com>
2023-10-29 18:19:04 +0900

    update

--- summarise_split_text.py
+++ summarise_split_text.py
@@ -28,8 +28,13 @@ def ask_using_file(file: str) -> str:
 
 def ask_a_question(question: str) -> str:
     key_file = Path().home() / "Developer/api_keys/openai.txt"
+
+    if not key_file.exists():
+        raise FileNotFoundError("api key not found.")
+
     text = key_file.read_text()
     openai.api_key = re.match(r'OPENAI_API_KEY="(.*)"', text).group(1)
+
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
