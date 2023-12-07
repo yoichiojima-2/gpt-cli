b57a3195f125fe7f0080c518fa8fef48874b48c2
Yoichi Ojima <yoichiojima@gmail.com>
2023-12-07 21:10:23 +0900

    make it interactive

--- chatgpt/ask_a_question.py
+++ chatgpt/ask_a_question.py
@@ -1,22 +1,48 @@
+import os
 import re
-from pathlib import Path
 import openai
+import logging
 
 
-def ask_a_question(question: str) -> str:
-    key_file = Path().home() / "Developer/keys/openai.txt"
+def authenticate():
+    api_key = os.environ.get("OPENAI_API_KEY")
+    if not api_key:
+        raise EnvironmentError("OpenAI API key not found in environment variables.")
+    openai.api_key = api_key
 
-    if not key_file.exists():
-        raise FileNotFoundError("api key not found.")
 
-    text = key_file.read_text()
-    openai.api_key = re.match(r'OPENAI_API_KEY="(.*)"', text).group(1)
+def fetch_answer(message: list[dict[str, str]]) -> str:
+    try:
+        response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=message)
+        return response.get("choices")[0]["message"]
+    except Exception as e:
+        logging.error(f"Error fetching answer: {e}")
+        return "Sorry, I couldn't fetch a response."
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "user", "content": question},
-        ],
-    )
 
-    return response["choices"][0]["message"]["content"]
+def main():
+    context = []
+    authenticate()
+
+    try:
+        while True:
+            try:
+                question = input("You: ")
+                context.append({"role": "user", "content": question})
+                res = fetch_answer(context)
+                print("ChatGPT: ", res)
+                context.append({"role": "assistant", "content": res})
+
+                if len(context) > 20:  # limit context size
+                    context = context[-20:]
+            except EOFError:
+                print("\nExiting the application. Goodbye!")
+                break
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        print("Application closed.")
+
+
+if __name__ == "__main__":
+    main()
--- chatgpt/cli.py
+++
@@ -1,8 +0,0 @@
-from argparse import ArgumentParser
-from .main import main
-
-def entrypoint():
-    parser = ArgumentParser()
-    parser.add_argument("--question", "-q", type = str)
-    args = parser.parse_args()
-    main(args.question)
