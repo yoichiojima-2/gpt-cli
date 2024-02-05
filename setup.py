8f0b716240b7dd8fe71e2c94a5b0f0f1e38baed3
Yoichi Ojima <yoichiojima@gmail.com>
2024-02-05 21:27:00 +0900

    remove everything
    - remove everything before recreate as poetry project

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="chatgpt",
-    version="2023.10",
-    packages=find_packages(),
-    install_requires=["openai"],
-    entry_points={"console_scripts": ["chatgpt = chatgpt.cli:entrypoint"]},
-)
